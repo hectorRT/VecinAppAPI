9efb2d4805551dc9c6a1e9dd2234ae26bc04935f
EstebanMenaUCNE <esteban_mena@ucne.edu.do>
2018-07-12 15:57:48 -0400

    Agregué tabla Estados a Script DB, agregué metodo getEstados en Modelo Discusiones, Arreglé identación modelo Discusiones

--- src/Scripts/DbScripts.sql
+++ src/Scripts/DbScripts.sql
@@ -2,27 +2,32 @@ CREATE DATABASE VecinAppDb;
 
 USE VecinAppDb;
 
+CREATE TABLE IF NOT EXISTS Estados(
+    IdEstado INT PRIMARY KEY AUTO_INCREMENT,
+    Descripcion VARCHAR (100) NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS Discusiones(
-IdDiscusion INT PRIMARY KEY AUTO_INCREMENT,
-IdVecino INT DEFAULT 0,
-Titulo VARCHAR(100) NOT NULL,
-Descripcion VARCHAR(500) NOT NULL,
-Conclusion VARCHAR(500) DEFAULT '',
-Estado TINYINT(1) NOT NULL,
-FechaCreacion DATETIME DEFAULT NOW(),
-ModifyBy INT default 0,
-DateModification datetime default now()
+    IdDiscusion INT PRIMARY KEY AUTO_INCREMENT,
+    IdVecino INT DEFAULT 0,
+    Titulo VARCHAR(100) NOT NULL,
+    Descripcion VARCHAR(500) NOT NULL,
+    Conclusion VARCHAR(500) DEFAULT '',
+    Estado TINYINT(1) NOT NULL,
+    FechaCreacion DATETIME DEFAULT NOW(),
+    ModifyBy INT default 0,
+    DateModification datetime default now()
 );
 
 CREATE TABLE IF NOT EXISTS DiscusionComentarios(
-IdComentario INT PRIMARY KEY AUTO_INCREMENT,
-IdDiscusion INT NOT NULL,
-IdVecino INT DEFAULT 0,
-Fecha DATETIME NOT NULL,
-Comentario VARCHAR(500),
-ModifyBy INT default 0,
-DateModification datetime default now(),
-FOREIGN KEY (IdDiscusion) REFERENCES Discusiones(IdDiscusion)
+    IdComentario INT PRIMARY KEY AUTO_INCREMENT,
+    IdDiscusion INT NOT NULL,
+    IdVecino INT DEFAULT 0,
+    Fecha DATETIME NOT NULL,
+    Comentario VARCHAR(500),
+    ModifyBy INT default 0,
+    DateModification datetime default now(),
+    FOREIGN KEY (IdDiscusion) REFERENCES Discusiones(IdDiscusion)
 );
 
 CREATE TABLE IF NOT EXISTS Vecinos(
