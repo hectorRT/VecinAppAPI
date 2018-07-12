387785feb9cdffddbbaccdb4bbb69d9380eb351f
robert0329 <robert_frias2@ucne.edu.do>
2018-07-12 10:41:50 -0700

    Routes

--- src/Models/Solicitudes.js
+++ src/Models/Solicitudes.js
@@ -1,102 +1,84 @@
 const DbConnection = require('../Connection/DbConnection');
+const conexion = DbConnection();
+function MetodosDB(){
 
-let SolicitudModel = {};
-
-const connection = DbConnection();
-
-SolicitudModel.getSolicitudes = (callback) => {
-    if(connection){
-        connection.query('SELECT * FROM Solicitudes ORDER BY Fecha DESC',
-    (err, rows) => {
-        if(err)
-        {
-            callback(err,null);
-        }else{
-            callback(null,rows);
-        }
-    })
-    }
-};
-
-SolicitudModel.getSolicitudes = (Id,callback) => {
-    if(connection){
-        connection.query('SELECT * FROM Solicitudes WHERE Id = ?', Id,
-    (err, rows) => {
-        if(err)
-        {
-            callback(err,null);
-        }else{
-            callback(null,rows);
-        }
-    })
-    }
-};
-
-SolicitudModel.insertSolicitud= (discusion, callback) => {
-    if(connection){
-        connection.query('INSERT INTO Solicitudes SET ?', discusion,
-            (err, result) => {
-                if(err){
-                    callback(err, {'insertId': 0});
-                }else{
-                    callback(null, {'insertId': result.insertId});
+    this.seleccionar = function(respuesta){
+        conexion.query('select * from solicitudes', function(error,resultado){
+                
+                if(error){
+                    respuesta.send({estado: 'Error'})
+                }else
+                {
+                    respuesta.send(resultado);
                 }
-            });
-    }
-};
-
-SolicitudModel.updateSolicitud = (Data, callback) => {
-    if (connection) {
-      const sql = `
-        UPDATE Solicitudes SET
-        Fecha = ${connection.escape(Data.Titulo)},
-        Tema = ${connection.escape(Data.Descripcion)},
-        Descripcion = ${connection.escape(Data.Conclusion)},
-        ModifyBy = ${connection.escape(Data.ModifyBy)},
-        DateModification = ${connection.escape(Data.DateModification)}
-        WHERE id = ${userData.id}`;
-  
-      connection.query(sql, function (err, result) {
-        if (err) {
-          callback(err,null);
-        } else {
-          callback(null, {
-            "msg": "success"
-          });
-        }
-      });
-    }
-  };
-
-  SolicitudModel.deleteDiscusion = (id, callback) => {
-if (connection) {
-    var sqlExists = `
-    SELECT * FROM Solicitudes WHERE Id = ${connection.escape(id)}
-    `;
-
-    connection.query(sqlExists, (err, row) => {
-    if (row) {
-        var sql = `DELETE FROM Solicitudes WHERE Id=` + connection.escape(id);
+                
+            })
         
-        connection.query(sql, (err, result) => {
-        if (err) {
-            callback(err,null);
-        } else{
-            callback(null, {
-            "msg": "deleted"
-            });
-        }
-        });
-
-    } else {
-
-        callback(null, {
-        "msg": "not Exists"
-        });
-
     }
-    });
-}
-};
 
-module.exports = SolicitudModel;
+    this.seleccionarId = function(id,respuesta){
+        conexion.query('select * from solicitudes where id=?',id, function(error,resultado){
+                
+                if(error){
+                    respuesta.send({estado:'error'});
+                }else{
+                    respuesta.send(resultado);
+                }
+                
+            })
+        
+    
+    
+    }
+    this.seleccionarNombre = function(nombre,respuesta){
+        conexion.query('select * from solicitudes where nombre=?',nombre, function(error,resultado){
+                
+                if(error){
+                    respuesta.send({estado:'error'});
+                }else{
+                    respuesta.send(resultado);
+                }
+                
+            })
+        
+    
+    
+    }
+    this.insertar = function(datos, respuesta){
+        conexion.query('insert into solicitudes set ?', datos, function(error,resultado){
+                
+                if(error){
+                    respuesta.send({estado:'Error'});
+                }else{
+                    respuesta.send({estado: 'Ok'});
+                }
+            })
+        
+    }
+    this.actualizar = function(datos, respuesta){
+        conexion.query('update solicitudes set ? where id = ?', [datos,datos.id], function(error,resultado){
+                
+                if(error){
+                    respuesta.send({estado:'Error'});
+                }else{
+                    respuesta.send({estado: 'Ok'});
+                }
+            })
+        
+    }
+    this.borrar = function(id, respuesta){
+        conexion.query('delete from solicitudes where id = ?', [id], function(error,resultado){
+                
+                if(error){
+                    respuesta.send({estado:'Error'});
+                }else{
+                    respuesta.send({estado: 'Ok'});
+                }
+            })
+        
+    }
+}
+
+   
+
+module.exports = new MetodosDB();
