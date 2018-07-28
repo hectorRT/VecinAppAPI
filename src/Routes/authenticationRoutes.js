08bbe2f63f0e73626843f7f06e1617783d3aa081
robert0329 <robert_frias2@ucne.edu.do>
2018-07-27 21:08:26 -0700

    Login full

--- src/Routes/authenticationRoutes.js
+++ src/Routes/authenticationRoutes.js
@@ -1,7 +1,6 @@
 const Solicitud = require('../Models/authenticate');
 
 module.exports = function(app){
-<<<<<<< HEAD
 
     // app.get('/authentication/', function(solicitud, respuesta){
     //     Solicitud.GetToken(respuesta);
@@ -11,16 +10,7 @@ module.exports = function(app){
     // })
     app.get('/authentication/:email/', function(solicitud, respuesta){
         Solicitud.authentication(solicitud.params.email, respuesta);
-=======
-    app.get('/authentication/', function(solicitud, respuesta){
-        Solicitud.seleccionar(respuesta);
-    })
-    /*app.get('/authentication/:id/', function(solicitud, respuesta){
-        Solicitud.seleccionarId(solicitud.params.id, respuesta);
-    })*/
-    app.get('/authentication/:Email/', function(solicitud, respuesta){
-        Solicitud.authentication(solicitud.params.Email, respuesta);
->>>>>>> 6b576589e85701404610bb0509a030a0499b50ca
+
     })
     app.get('/authenticationt/:token/', function(solicitud, respuesta){
         Solicitud.Obtenerdatos(solicitud.params.token, respuesta);
